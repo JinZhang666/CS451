379efa7a256dd36a6ae6f45475bc71873ad30713
Kimberly L <kimberly.jinn@gmail.com>
2018-08-17 14:39:23 -0400

    Another version of the Online Checkers project
    Recreated another version of Online Checkers project in the shared repo

---
+++ Online_Checkers/src/boardGame/Checkers.java
@@ -0,0 +1,125 @@
+package boardGame;
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.EtchedBorder;
+import java.awt.*;
+
+public class Checkers {
+
+	public static void main(String[] args) {
+		setUpGameWindowBoardAndPieces();
+	}
+
+	public static void setUpGameWindowBoardAndPieces(){
+		Board mainBoard;
+		int numOfRows = 8;
+		int numOfColumns = 8;
+		int numOfPlayerOnePieces = 12;
+		int numOfPlayerTwoPieces = 12;
+		JLabel playerOneLabel = new JLabel("Player 1");
+		JLabel playerTwoLabel = new JLabel("Player 2");
+		JPanel boardDisplayPanel;
+		JPanel leftPanel = new JPanel();
+		JPanel rightPanel = new JPanel();
+		Border lowerEtched = BorderFactory.createEtchedBorder(EtchedBorder.LOWERED);
+		JButton forfeitButton = new JButton("FORFEIT GAME");
+		
+		// Creating the main Checkers board
+		mainBoard = new Board(numOfRows, numOfColumns);
+		
+		/* 
+		 * The board is drawn in a Swing JPanel, but we have to get this JPanel 
+		 * and put it into our own GUI which is done by...
+		 */
+		boardDisplayPanel = mainBoard.getJPanel(); // main Checkers board display
+		boardDisplayPanel.setMinimumSize(new Dimension(450, 450));
+		boardDisplayPanel.setPreferredSize(new Dimension(450, 450));
+		
+		// Creating the 2 sets of Checkers pieces
+		Piece[] playerOnePieces = new Piece[numOfPlayerOnePieces];
+		Piece[] playerTwoPieces = new Piece[numOfPlayerTwoPieces];
+		for(int i = 0; i < numOfPlayerOnePieces; i++){
+			playerOnePieces[i] = new RoundPiece(new Color(255, 255, 204));
+		}
+		for(int i = 0; i < numOfPlayerTwoPieces; i++){
+			playerTwoPieces[i] = new RoundPiece(new Color(210, 180, 140));
+		}
+	
+		// Setting the Checkers pieces onto the board
+		playerOnePieces[0].place(mainBoard, 0, 1); playerOnePieces[1].place(mainBoard, 0, 3);
+		playerOnePieces[2].place(mainBoard, 0, 5); playerOnePieces[3].place(mainBoard, 0, 7);
+		playerOnePieces[4].place(mainBoard, 1, 0); playerOnePieces[5].place(mainBoard, 1, 2);
+		playerOnePieces[6].place(mainBoard, 1, 4); playerOnePieces[7].place(mainBoard, 1, 6);
+		playerOnePieces[8].place(mainBoard, 2, 1); playerOnePieces[9].place(mainBoard, 2, 3);
+		playerOnePieces[10].place(mainBoard, 2, 5); playerOnePieces[11].place(mainBoard, 2, 7);
+				
+		playerTwoPieces[0].place(mainBoard, 5, 0); playerTwoPieces[1].place(mainBoard, 5, 2);
+		playerTwoPieces[2].place(mainBoard, 5, 4); playerTwoPieces[3].place(mainBoard, 5, 6);
+		playerTwoPieces[4].place(mainBoard, 6, 1); playerTwoPieces[5].place(mainBoard, 6, 3);
+		playerTwoPieces[6].place(mainBoard, 6, 5); playerTwoPieces[7].place(mainBoard, 6, 7);
+		playerTwoPieces[8].place(mainBoard, 7, 0); playerTwoPieces[9].place(mainBoard, 7, 2);
+		playerTwoPieces[10].place(mainBoard, 7, 4); playerTwoPieces[11].place(mainBoard, 7, 6);
+		
+		// Creating/setting up the main game window
+		JFrame mainFrame = new JFrame("Online Checkers");
+		mainFrame.setSize(700, 600);
+		
+		/*
+		 * Setting up the components on the window:
+		 * 		Left Panel (which will contain 3 components) and the Right Panel 
+		 * 		(which will contain buttons)
+		 */
+		Container pane = mainFrame.getContentPane();
+		GridBagLayout gblOne = new GridBagLayout();
+		GridBagLayout gblTwo = new GridBagLayout();
+		GridBagConstraints gbcOne = new GridBagConstraints();
+		GridBagConstraints gbcTwo = new GridBagConstraints();
+		pane.setLayout(gblOne);
+		gbcOne.fill = GridBagConstraints.HORIZONTAL;
+		
+		leftPanel.setBorder(lowerEtched);		
+		gbcOne.weightx = 0.75;
+		gbcOne.weighty = 1.0;
+		gbcOne.gridx = 0;
+		gbcOne.gridy = 0;
+		gblOne.setConstraints(leftPanel, gbcOne);
+		pane.add(leftPanel);
+		
+		rightPanel.setBorder(lowerEtched);
+		gbcOne.weightx = 0.25;
+		gbcOne.weighty = 1.0;
+		gbcOne.gridx = 1;
+		gbcOne.gridy = 0;
+		gblOne.setConstraints(rightPanel, gbcOne);
+		pane.add(rightPanel);
+		
+		leftPanel.setLayout(gblTwo);
+		gbcTwo.fill = GridBagConstraints.VERTICAL;
+		
+		gbcTwo.weighty = 0.10;
+		gbcTwo.weightx = 1.0;
+		gbcTwo.gridx = 0;
+		gbcTwo.gridy = 0;
+		gblTwo.setConstraints(playerOneLabel, gbcTwo);
+		leftPanel.add(playerOneLabel);
+		
+		gbcTwo.weighty = 0.80;
+		gbcTwo.weightx = 1.0;
+		gbcTwo.gridx = 0;
+		gbcTwo.gridy = 1;
+		gblTwo.setConstraints(boardDisplayPanel, gbcTwo);
+		leftPanel.add(boardDisplayPanel);
+		
+		gbcTwo.weighty = 0.10;
+		gbcTwo.weightx = 1.0;
+		gbcTwo.gridx = 0;
+		gbcTwo.gridy = 2;
+		gblTwo.setConstraints(playerTwoLabel, gbcTwo);
+		leftPanel.add(playerTwoLabel);
+		
+		forfeitButton.setFocusable(false);
+		rightPanel.add(forfeitButton);
+		
+		mainFrame.setVisible(true);
+	}
+}
---
+++ Online_Checkers/src/boardGame/RoundPiece.java
@@ -0,0 +1,65 @@
+package boardGame;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+/**
+ * Example playing piece. This class is only here as an example,
+ * and may be discarded.
+ */
+public class RoundPiece extends Piece {
+    
+    private Color color = Color.red;
+        
+    /**
+     * Constructs a <code>RoundPiece</code>.
+     **/
+    public RoundPiece() {
+    }
+    
+    /**
+     * Constructs a <code>RoundPiece</code> of the given color.
+     * 
+     * @param color The <code>Color</code> of the new piece.
+     **/
+     public RoundPiece(Color color) {
+        this.color = color;
+    }
+     
+     /**
+      * Constructs a <code>RoundPiece</code> with the given name and color.
+      * 
+      * @param name A name for the new Piece.
+      * @param color The <code>Color</code> of the new Piece.
+      **/
+     public RoundPiece(String name, Color color) {
+         super(name);
+         this.color = color;
+     }
+    
+    /**
+     * Draws this <code>RoundPiece</code> on the given <code>Graphics</code>.
+     * Drawing should be limited to the provided <code>java.awt.Rectangle</code>.
+     * 
+     * @param g The graphics on which to draw.
+     * @param r The rectangle in which to draw.
+     */
+    @Override
+    public void paint(Graphics g, Rectangle r) {
+        // black border to differentiate round pieces
+    	g.setColor(Color.BLACK);
+    	g.fillOval(r.x, r.y, r.width, r.height);
+    	
+    	Color oldColor = g.getColor();
+        g.setColor(color);
+        g.fillOval(r.x + 1, r.y + 1, r.width - 2, r.height - 2);
+        if (this.equals(board.getSelectedPiece())) {
+            g.setColor(Color.RED);
+            g.drawOval(r.x + 1, r.y + 1, r.width - 2, r.height - 2);
+            g.setColor(Color.WHITE);
+            g.drawOval(r.x + 2, r.y + 2, r.width - 4, r.height - 4);
+        }
+        g.setColor(oldColor);
+    }
+}
