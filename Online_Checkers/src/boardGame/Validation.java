0865ce672de3cbf0fffd8d7a302831867d556077
mzhong1 <mzhong.phiria@yahoo.com>
2018-08-22 17:34:47 -0400

    Validation Code
    So far, isValidMove can check for a regular piece's move (just playerone for now, gotta add PlayerTwo and King piece move validation)

--- Online_Checkers/src/boardGame/Validation.java
+++ Online_Checkers/src/boardGame/Validation.java
@@ -1,15 +1,132 @@
 package boardGame;
 
 public class Validation {
-	public static boolean isValidMove(Piece p, Board b, int playernum, int r, int c)
+	
+	public static boolean canMakeJump(Piece p, Board b, int r, int c)
 	{
 		return false;
 	}
-	
-	private static boolean isDiagonal(int piecer, int piecec, int mover, int movec)
+	public static boolean isValidMove(Piece p, Board b, int r, int c)
 	{
-		return (mover == piecer + 1 && movec == piecec + 1) || (mover == piecer - 1 && movec == piecec + 1) || (mover == piecer + 1 && movec == piecec - 1) || (mover == piecer - 1 && movec == piecec - 1);
+		int prow = p.getRow();
+		int pcol = p.getColumn();
+		if(p.isRegularPiece())
+		{
+			if(p.getName().equals("PlayerOne"))
+			{
+				//if moving diagonally forward -> down the column
+				if(isDiagonal(prow, pcol, r, c) && pcol > c)
+				{
+					return true;
+				}
+				else if(isValidRegularJump(p, b, r, c)) //check if the diagonal move was a valid jump;
+				{
+					return true;
+				}
+				else 
+				{
+					return false;
+				}
+			}
+			else if(p.getName().equals("PlayerTwo"))
+			{
+				
+			}
+		}
+		return false;
 	}
 	
-	private static boolean isValidRegularJump();
+	/*
+	 * isDiagonal: int * int * int * int -> boolean
+	 * REQUIRES: prow, pcol, mrow, mcol are not out of bounds
+	 * ENSURES: returns true if the move coordintates are diagonal by 1 square to the current player position
+	 * */
+	private static boolean isDiagonal(int prow, int pcol, int mrow, int mcol)
+	{
+		return (mrow == prow + 1 && mcol == pcol + 1) || (mrow == prow - 1 && mcol == pcol + 1) || (mrow == prow + 1 && mcol == pcol - 1) || (mrow == prow - 1 && mcol == pcol - 1);
+	}
+	
+	/*
+	 * isJumpCoord: int * int * int * int -> bool
+	 * REQUIRES: all inputs are in the board bounds
+	 * ENSURES: returns true if the move coordinates are a valid jump coordinate (Not if the jump is valid, just that the
+	 * coordinates are valid) false otherwise.
+	 * */
+	private static boolean isJumpCoord(int prow, int pcol, int mrow, int mcol)
+	{
+		return (mrow == prow + 2 && mcol == pcol + 2) || (mrow == prow - 2 && mcol == pcol + 2) || (mrow == prow + 2 && mcol == pcol - 2) || (mrow == prow - 2 && mcol == pcol - 2);
+	}
+	
+	/*
+	 * isValidRegularJump: Board * Piece * int * int -> bool
+	 * REQUIRES: Board b != NULL, Piece p != NULL, mrow, mcol are within the bounds of the board.  
+	 * ENSURES: returns true if the move is a proper regular jump, otherwise, return false
+	 * */
+	private static boolean isValidRegularJump(Piece p, Board b, int mrow, int mcol)
+	{
+		int prow = p.getRow();
+		int pcol = p.getColumn();
+		if(p.getName().equals("PlayerOne")) //their pieces go DOWN (aka + numbers)
+		{
+			if(!isJumpCoord(prow, pcol, mrow, mcol) || mcol < pcol) //if piece is not moving "down" (move is < current pos)
+			{
+				return false;
+			}
+			//get the captured piece
+			Piece capture;
+			if(mrow > prow)
+			{
+				capture = b.getPiece(mrow -1, mcol - 1);
+			}
+			else
+			{
+				capture = b.getPiece(mrow + 1, mcol - 1);
+			}
+			//Check if capture piece belongs to Playertwo
+			if(!capture.getName().equals("PlayerTwo"))
+			{
+				return false;
+			}
+			//check if the piece on the board where the player will move is empty
+			Piece jump = b.getPiece(mrow, mcol);
+			if(jump != null || jump.isRegularPiece())
+			{
+				return false;
+			}
+			return true;
+		}
+		else if(p.getName().equals("PlayerTwo"))//player == playerTwo
+		{
+			if(!isJumpCoord(prow, pcol, mrow, mcol) || mcol > pcol) //if piece is not moving "up" (move is > current pos)
+			{
+				return false;
+			}
+			//get the captured piece
+			Piece capture;
+			if(mrow > prow)
+			{
+				capture = b.getPiece(mrow - 1, mcol + 1);
+			}
+			else
+			{
+				capture = b.getPiece(mrow + 1, mcol + 1);
+			}
+			//Check if capture piece belongs to PlayerOne
+			if(!capture.getName().equals("PlayerOne"))
+			{
+				return false;
+			}
+			//check if the piece on the board where the player will move is empty
+			Piece jump = b.getPiece(mrow, mcol);
+			if(jump != null)
+			{
+				return false;
+			}
+			return true;
+		}
+		else //the input piece belongs to neither the playerOne or PlayerTwo
+		{
+			return false;
+		}
+	}
 }
