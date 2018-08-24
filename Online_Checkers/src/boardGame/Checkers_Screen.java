830df73bc8db15e713bc2bf8583cb613ebbfb6b0
mzhong1 <mzhong.phiria@yahoo.com>
2018-08-24 17:59:26 -0400

    Revert "Created setUpBoard method and start on the playGame method"
    This reverts commit fc6f2d3092ece4a994c68dff51f24b0e797dae3a.

--- Online_Checkers/src/boardGame/Checkers_Screen.java
+++ Online_Checkers/src/boardGame/Checkers_Screen.java
@@ -6,51 +6,15 @@
  * Description: This window displays the Checkers game where both players are playing on.
  */
 package boardGame;
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JPanel;
+import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.border.EtchedBorder;
-import java.awt.EventQueue;
-import java.awt.Color;
-import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.Rectangle;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.Graphics;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.util.Observable;
-import java.util.Observer;
+import java.awt.*;
+import java.awt.event.*;
 
 public class Checkers_Screen {
 
-	static JFrame frame;
-	static Board mainBoard;
-	int numOfRows = 8;
-	int numOfColumns = 8;
-	static final int numOfPlayerPieces = 12;
-	String playerOne = "PlayerOne";
-	String playerTwo = "PlayerTwo";
-	static Piece[] playerOnePieces = new Piece[numOfPlayerPieces];
-	static Piece[] playerTwoPieces = new Piece[numOfPlayerPieces];
-	JLabel playerOneLabel = new JLabel("PLAYER 1");
-	JLabel playerTwoLabel = new JLabel("PLAYER 2");
-	JPanel boardDisplayPanel;
-	JPanel leftPanel = new JPanel();
-	JPanel rightPanel = new JPanel();
-	Border lowerEtched = BorderFactory.createEtchedBorder(EtchedBorder.LOWERED);
-	JButton forfeitButton = new JButton("FORFEIT GAME");
-	
-	static int currentPlayer = 1; // makes the Player One the player to start the game off
-	static boolean gameEnd = true;
+	JFrame frame;
 	
 	public static void main(String args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -66,41 +30,28 @@ public class Checkers_Screen {
 	}
 	
 	public Checkers_Screen(){
-		setUpBoard(mainBoard);
 		initialize();
-		playGame();
-	}
-	
-	public void setUpBoard(Board b){
-		// Creating the main Checkers board
-		mainBoard = new Board(numOfRows, numOfColumns);
-	
-		// Creating the 2 sets of Checkers pieces and names them either
-		// "playerOne" or "playerTwo"	
-		for(int i = 0; i < numOfPlayerPieces; i++){
-			playerOnePieces[i] = new RoundPiece(playerOne, new Color(255, 255, 204));
-		}
-		for(int i = 0; i < numOfPlayerPieces; i++){
-			playerTwoPieces[i] = new RoundPiece(playerTwo, new Color(210, 180, 140));
-		}
-			
-		// Setting the Checkers pieces onto the board
-		playerOnePieces[0].place(mainBoard, 0, 1); playerOnePieces[1].place(mainBoard, 0, 3);
-		playerOnePieces[2].place(mainBoard, 0, 5); playerOnePieces[3].place(mainBoard, 0, 7);
-		playerOnePieces[4].place(mainBoard, 1, 0); playerOnePieces[5].place(mainBoard, 1, 2);
-		playerOnePieces[6].place(mainBoard, 1, 4); playerOnePieces[7].place(mainBoard, 1, 6);
-		playerOnePieces[8].place(mainBoard, 2, 1); playerOnePieces[9].place(mainBoard, 2, 3);
-		playerOnePieces[10].place(mainBoard, 2, 5); playerOnePieces[11].place(mainBoard, 2, 7);
-				
-		playerTwoPieces[0].place(mainBoard, 5, 0); playerTwoPieces[1].place(mainBoard, 5, 2);
-		playerTwoPieces[2].place(mainBoard, 5, 4); playerTwoPieces[3].place(mainBoard, 5, 6);
-		playerTwoPieces[4].place(mainBoard, 6, 1); playerTwoPieces[5].place(mainBoard, 6, 3);
-		playerTwoPieces[6].place(mainBoard, 6, 5); playerTwoPieces[7].place(mainBoard, 6, 7);
-		playerTwoPieces[8].place(mainBoard, 7, 0); playerTwoPieces[9].place(mainBoard, 7, 2);
-		playerTwoPieces[10].place(mainBoard, 7, 4); playerTwoPieces[11].place(mainBoard, 7, 6);
 	}
 	
 	public void initialize(){
+		Board mainBoard;
+		int numOfRows = 8;
+		int numOfColumns = 8;
+		int numOfPlayerOnePieces = 12;
+		int numOfPlayerTwoPieces = 12;
+		String playerOne = "playerOne";
+		String playerTwo = "playerTwo";
+		JLabel playerOneLabel = new JLabel("PLAYER 1");
+		JLabel playerTwoLabel = new JLabel("PLAYER 2");
+		JPanel boardDisplayPanel;
+		JPanel leftPanel = new JPanel();
+		JPanel rightPanel = new JPanel();
+		Border lowerEtched = BorderFactory.createEtchedBorder(EtchedBorder.LOWERED);
+		JButton forfeitButton = new JButton("FORFEIT GAME");
+		
+		// Creating the main Checkers board
+		mainBoard = new Board(numOfRows, numOfColumns);
+		
 		/* 
 		 * The board is drawn in a Swing JPanel, but we have to get this JPanel 
 		 * and put it into our own GUI which is done by...
@@ -108,6 +59,32 @@ public class Checkers_Screen {
 		boardDisplayPanel = mainBoard.getJPanel(); // main Checkers board display
 		boardDisplayPanel.setMinimumSize(new Dimension(450, 450));
 		boardDisplayPanel.setPreferredSize(new Dimension(450, 450));
+		
+		// Creating the 2 sets of Checkers pieces and names them either
+		// "playerOne" or "playerTwo"
+		Piece[] playerOnePieces = new Piece[numOfPlayerOnePieces];
+		Piece[] playerTwoPieces = new Piece[numOfPlayerTwoPieces];
+		for(int i = 0; i < numOfPlayerOnePieces; i++){
+			playerOnePieces[i] = new RoundPiece(playerOne, new Color(255, 255, 204));
+		}
+		for(int i = 0; i < numOfPlayerTwoPieces; i++){
+			playerTwoPieces[i] = new RoundPiece(playerTwo, new Color(210, 180, 140));
+		}
+	
+		// Setting the Checkers pieces onto the board
+		playerOnePieces[0].place(mainBoard, 0, 1); playerOnePieces[1].place(mainBoard, 0, 3);
+		playerOnePieces[2].place(mainBoard, 0, 5); playerOnePieces[3].place(mainBoard, 0, 7);
+		playerOnePieces[4].place(mainBoard, 1, 0); playerOnePieces[5].place(mainBoard, 1, 2);
+		playerOnePieces[6].place(mainBoard, 1, 4); playerOnePieces[7].place(mainBoard, 1, 6);
+		playerOnePieces[8].place(mainBoard, 2, 1); playerOnePieces[9].place(mainBoard, 2, 3);
+		playerOnePieces[10].place(mainBoard, 2, 5); playerOnePieces[11].place(mainBoard, 2, 7);
+		
+		playerTwoPieces[0].place(mainBoard, 5, 0); playerTwoPieces[1].place(mainBoard, 5, 2);
+		playerTwoPieces[2].place(mainBoard, 5, 4); playerTwoPieces[3].place(mainBoard, 5, 6);
+		playerTwoPieces[4].place(mainBoard, 6, 1); playerTwoPieces[5].place(mainBoard, 6, 3);
+		playerTwoPieces[6].place(mainBoard, 6, 5); playerTwoPieces[7].place(mainBoard, 6, 7);
+		playerTwoPieces[8].place(mainBoard, 7, 0); playerTwoPieces[9].place(mainBoard, 7, 2);
+		playerTwoPieces[10].place(mainBoard, 7, 4); playerTwoPieces[11].place(mainBoard, 7, 6);
 		
 		// Creating/setting up the main game window
 		frame = new JFrame("Online Checkers");
@@ -145,7 +122,7 @@ public class Checkers_Screen {
 		});
 		instructions.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
-				Instructions_Screen is = new Instructions_Screen();
+				In_Game_Instructions_Screen is = new In_Game_Instructions_Screen();
 				is.frame.setVisible(true);
 			}
 		});
@@ -206,23 +183,4 @@ public class Checkers_Screen {
 		
 		frame.setVisible(true);
 	}
-	
-	public static void playGame(){
-		if(currentPlayer == 1){
-			for(int i = 0; i < numOfPlayerPieces; i++){
-				playerOnePieces[i].setDraggable(true);
-				playerTwoPieces[i].setDraggable(false);
-				//System.out.println("    draggable = " + playerOnePieces[i].isDraggable());
-				//System.out.println("    draggable = " + playerTwoPieces[i].isDraggable());
-			}
-			//currentPlayer = 2;
-		}
-		if(currentPlayer == 2){
-			for(int i = 0; i < numOfPlayerPieces; i++){
-				playerOnePieces[i].setDraggable(false);
-				playerTwoPieces[i].setDraggable(true);
-			}
-			//currentPlayer = 1;
-		}
-    }
 }
